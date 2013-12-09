26a4abf1a16e0a32504022ecfd5a46dad3699b37
Adam Horne <ahorne@wordplaysoftware.com>
2013-12-09 16:19:50 -0500

    Added FriendlyId to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  slug       :string(255)
+#
+
 class Post < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  slug       :string(255)
+#
+
 require 'spec_helper'
 
 describe Post do
