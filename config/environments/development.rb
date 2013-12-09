32bbe2902d45fcd87d8653b98e334172f7c1b958
Adam Horne <ahorne@wordplaysoftware.com>
2013-12-09 15:05:59 -0500

    Changed module name to PrettyUrls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-TemplateBootstrapSite::Application.configure do
+PrettyUrls::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TemplateBootstrapSite::Application.config.session_store :cookie_store, key: '_template_bootstrap_site_session'
+PrettyUrls::Application.config.session_store :cookie_store, key: '_pretty_urls_session'
